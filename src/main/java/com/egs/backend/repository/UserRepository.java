32d5dadfef91be80bcfba02439dda28b6c29a757
Sat Kartashyan <satkartashyan@gmail.com>
2020-02-24 14:13:36 +0400

    Creating repository layer

---
+++ src/main/java/com/egs/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.egs.backend.repository;
+
+import com.egs.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByEmail(String email);
+    Collection<User> findAllByRole(String role);
+
+}
